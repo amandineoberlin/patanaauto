82df25afbec3243ed86bac534f9a48aa2516b98e
amandineoberlin <amandine.oberlin@planorama.com>
2019-09-14 11:56:07 +0200

    added logic for scheduler

--- www/patanaauto/app.js
+++ www/patanaauto/app.js
@@ -4,7 +4,9 @@ const http = require('http');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const schedule = require('node-schedule');
 
+const rule = new schedule.RecurrenceRule();
 const limit = '10mb';
 const port = 5001;
 const static_folder = path.join(__dirname, 'dist/patanaauto');
@@ -20,6 +22,10 @@ app.use('/', express.static(static_folder));
 
 require('./routes/annonces')(app);
  
+schedule.scheduleJob({ start: Date.now(), rule: '*/7' }, () => {
+  console.log('The answer to life, the universe, and everything!');
+});
+
 server.listen(port, function () {
     console.log('Node Express server for ' + app.name + ' listening on http://localhost:' + port);
 });
