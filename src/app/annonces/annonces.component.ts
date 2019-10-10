a1311817b98ccb2f0670f5bd18ad1e860d97a5c6
amandineoberlin <amandine.oberlin@planorama.com>
2019-10-10 13:05:45 +0200

    added ribbon

--- src/app/annonces/annonces.component.ts
+++ src/app/annonces/annonces.component.ts
@@ -38,6 +38,19 @@ export class AnnoncesComponent implements OnInit {
   initFromPrice: Number;
   initToPrice: Number;
   notFoundText: String = Constants.NOT_FOUND_MESSAGE;
+  limit: Number = 10;
+  filteredAnnonces: Object;
+
+  mainImage(annonce) {
+    return `../../assets/selsia-photos/${annonce.images[0]}`;
+  }
+
+  filterAnnonces(filter) {
+    if (!filter) {
+      this.filteredAnnonces = this.annonces;
+      return this.annonces;
+    };
+  }
 
   inputValue(el) {
     return el.data();
@@ -203,6 +216,9 @@ export class AnnoncesComponent implements OnInit {
     // hide price range slider when user clicks anywhere else than the input itself
     this.hideSlidersOnClick();
 
+    //@ts-ignore
+    $('.dropdown-toggle').dropdown()
+
     this.activatedRoute.queryParams.subscribe(params => {
       let marque = params['marque'];
       let modele = params['modele'];
@@ -210,7 +226,8 @@ export class AnnoncesComponent implements OnInit {
     });
 
     this.formDataService.loadAnnonces({ fullSearch: true })
-      .then(dataObj => _.assign(this, dataObj));
+      .then(dataObj => _.assign(this, dataObj))
+      .then(() => this.filterAnnonces(null));
   }
 
 }
