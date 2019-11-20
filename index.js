85ef9861fa0fec1ba9d9debc2408c54b24ec8098
amandineoberlin <amandine.oberlin@planorama.com>
2019-11-20 10:56:48 +0100

    added buildpack files

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> test!</p>'+
+'    </body>'+
+'</html>');
+    res.end();
+});
+server.listen(process.env.PORT || 8080);
