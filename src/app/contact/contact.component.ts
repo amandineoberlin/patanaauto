8e22660bfd30e48fc2e4217aa3ae5826e3ed64ea
amandineoberlin <amandine.oberlin@planorama.com>
2020-02-16 23:17:56 +0100

    added skeleton for contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  contactForm: FormGroup;
 
   ngOnInit() {
+    this.contactForm = this.fb.group({
+      nom: [null],
+      email: [null],
+      message: [null]
+    });
   }
 
 }
