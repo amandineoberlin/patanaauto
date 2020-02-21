4b53174157c971745c4e6998b39ef51a7ee0bd11
amandineoberlin <amandine.oberlin@planorama.com>
2020-02-21 18:57:55 +0100

    reformatted form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,4 @@
-import _ from 'lodash';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-
-import { FormDataService } from '../services/form-data.service';
 
 @Component({
   selector: 'app-contact',
@@ -11,45 +7,9 @@ import { FormDataService } from '../services/form-data.service';
 })
 export class ContactComponent implements OnInit {
 
-  constructor(
-    private fb: FormBuilder,
-    private formDataService: FormDataService
-  ) { }
-
-  submitting: Boolean = false;
-  rejected: Boolean = false;
-  accepted: Boolean = false;
-  contactForm: FormGroup;
-  name: '';
-  email: '';
-  subject: '';
-  message: '';
-
-  onSubmit(): Promise<any> {
-    const isValue = val => (_.size(val) ? true : false);
-
-    this.submitting = true;
-    return this.formDataService.contactForm(this.contactForm.value)
-      .then(({ rejected, accepted }) => {
-        this.rejected = isValue(rejected);
-        this.accepted = isValue(accepted);
-        this.submitting = false;
-      });
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.contactForm = new FormGroup({
-      'name': new FormControl(this.name, [
-        Validators.required,
-        Validators.minLength(3)
-      ]),
-      'email': new FormControl(this.email, Validators.required),
-      'subject': new FormControl(this.subject, Validators.required),
-      'message': new FormControl(this.message, [
-        Validators.required,
-        Validators.minLength(10),
-      ]),
-    });
   }
 
 }
