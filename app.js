ca40f28fa286408227c203c609dfdb4ae5fbea97
amandineoberlin <amandine.oberlin@planorama.com>
2020-03-11 19:51:44 +0100

    test

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
 'use strict';
 
-const express = require('express');
 const http = require('http');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const schedule = require('node-schedule');
-const logger = require('./modules/logger');
+const fs = require('fs');
 
-const limit = '10mb';
 const port = 5001;
-const static_folder = path.join(__dirname, 'dist/patanaauto');
 
-const app = express();
-const server = http.createServer(app);
-
-app.use(cookieParser());
-app.use(bodyParser.json({ limit }));
-app.use(bodyParser.urlencoded({ extended: true, limit }));
-
-app.use('/', express.static(static_folder));
-
-require('./routes/annonces')(app);
-require('./routes/emailer')(app);
-
-schedule.scheduleJob({ hour: 1, minute: 1, dayOfWeek: 0, start: Date.now() }, () =>
-  require('./modules/schedule-job').retrieveData());
-
-schedule.scheduleJob({ hour: 1, minute: 1, date: 1, start: Date.now() }, () =>
-  require('./modules/schedule-job').cleanData());
-
-server.listen(port, () => 
-  logger.info('Node Express server for ' + app.name + ' listening on http://localhost:' + port));
-
-app.emit('ready');
+fs.readFile('./index.html', function (err, html) {
+  if (err) {
+    throw err;
+  }
+  http.createServer(function (request, response) {
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    response.write(html);
+    response.end();
+  }).listen(port);
+});
