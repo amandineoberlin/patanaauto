ce5f418417ec5bf28ce095b1461637c4bc5cb90e
amandineoberlin <amandine.oberlin@planorama.com>
2020-06-29 16:43:24 +0200

    try with only dev flag

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,18 @@ module.exports = function(grunt) {
       frontend_build: {
         cwd: 'public/',
         command: 'ng build --prod'
+      },
+      dev_build: {
+        cwd: 'public/',
+        command: 'npm install --only=dev'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-exec');
-  grunt.registerTask('installProdDeps', ['exec:npm_install', 'exec:frontend_build']);
+  grunt.registerTask('installProdDeps', [
+    'exec:npm_install',
+    'exec:frontend_build',
+    'exec:dev_build']);
   grunt.registerTask('default', 'installProdDeps');
 };
