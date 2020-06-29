099847db2d296dc37dc3b2e5c1beb480b2a383c6
amandineoberlin <amandine.oberlin@planorama.com>
2020-06-29 16:14:45 +0200

    try to fix grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     exec: {
-      build_frontend: {
+      npm_install: {
         cwd: 'public/',
-        command: 'npm i && npm run build --prod'
-      },
+        command: 'npm i'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-exec');
-  grunt.registerTask('installDeps', ['exec:build_frontend']);
+  grunt.registerTask('installDeps', ['exec:npm_install']);
   grunt.registerTask('default', 'installDeps');
 };
