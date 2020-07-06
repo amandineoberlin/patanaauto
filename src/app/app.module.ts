d4a0408c80060c103d869881bee5d84a9d61a78e
amandineoberlin <amandine.oberlin@planorama.com>
2020-07-06 17:11:32 +0200

    added animation to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -39,6 +40,7 @@ import { SvgLoaderComponent } from './svg-loader/svg-loader.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     NgSelectModule,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state('open',
+      style({
+        'max-height': '500px',
+        'opacity': '1',
+        'visibility': 'visible'
+      })),
+      state('closed',
+      style({ 
+        'max-height': '0px',
+        'opacity': '0',
+        'visibility': 'hidden'
+      })),
+      transition('open => closed', [
+        animate('200ms ease-in-out',
+        style({
+          'opacity': '0'
+        })),
+        animate('300ms ease-in-out',
+        style({
+          'max-height': '0px'
+        })),
+        animate('50ms ease-in-out',
+        style({
+          'visibility': 'hidden'
+        }))
+      ]),
+      transition('closed => open', [
+        animate('1ms ease-in-out',
+        style({
+          'visibility': 'visible'
+        })),
+        animate('300ms ease-in-out',
+        style({
+          'max-height': '500px'
+        })),
+        animate('200ms ease-in-out',
+        style({
+          'opacity': '1'
+        }))
+      ])
+    ])
+  ]
 })
+
 export class NavbarComponent implements OnInit {
 
   constructor() { }
 
   isShown: boolean;
+
+  toggle() {
+    this.isShown = !this.isShown;
+  }
 
   ngOnInit() {
     this.isShown = false;
