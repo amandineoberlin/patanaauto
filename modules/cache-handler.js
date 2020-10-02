5552be4d66f08d22474162c6cff04fc5f42d2d76
amandineoberlin <amandine.oberlin@planorama.com>
2020-10-02 20:49:35 +0200

    immroved logging

--- modules/cache-handler.js
+++ modules/cache-handler.js
@@ -1,12 +1,13 @@
 const logger = require('./logger');
 
 const clearAllCaches = (res) => {
-  logger.info('requested cache cleaning');
+  logger.info('requested all server caches cleaning');
   res.setHeader("Clear-Site-Data", "\"cache\", \"cookies\", \"storage\"");
   res.send({ success: true });
 }
 
 const setNoCache = (app, res) => {
+  logger.info('requested setting no response header caches');
   res.setHeader('Surrogate-Control', 'no-store');
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
   res.setHeader('Pragma', 'no-cache');
