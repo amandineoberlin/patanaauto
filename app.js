9b34c187bee9ee706450446c72db02fc5c38db78
amandineoberlin <amandine.oberlin@traxretail.com>
2021-05-25 14:29:52 +0200

    upload photos

--- app.js
+++ app.js
@@ -4,24 +4,22 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cron = require('node-cron');
 const compression = require('compression');
 
 const logger = require('./modules/logger');
-const scheduler = require('./modules/scheduler');
 
 const limit = '10mb';
 const port = 5001;
 const static_folder = path.join(__dirname, 'src/dist/patanaauto');
 
 const app = express();
-app.use(compression());
-const server = http.createServer(app);
+app
+  .use(express.urlencoded({ extended: true, limit }))
+  .use(compression())
+  .use(express.json())
+  .use(cookieParser());
 
-app.use(cookieParser());
-app.use(bodyParser.json({ limit }));
-app.use(bodyParser.urlencoded({ extended: true, limit }));
+const server = http.createServer(app);
 
 app.use('/', express.static(static_folder));
 
@@ -37,34 +35,6 @@ require('./routes/annonces')(app);
 require('./routes/emailer')(app);
 
 require('./modules/async-exists');
-
-// schedule ftp load every day at 23:00pm
-// usage is not nice but when using cron.schedule('59 23 * * *') it didnt work on heroku
-// although it did work locally. So this is a workaround
-cron.schedule('30 01 * * mon', async () => {
-  logger.info(`Daily FTP job scheduler launched! Date: ${new Date()}`);
-  await scheduler.launch();
-});
-cron.schedule('30 01 * * tue', async () => {
-  logger.info(`Daily FTP job scheduler launched! Date: ${new Date()}`);
-  await scheduler.launch();
-});
-cron.schedule('30 01 * * wed', async () => {
-  logger.info(`Daily FTP job scheduler launched! Date: ${new Date()}`);
-  await scheduler.launch();
-});
-cron.schedule('30 01 * * fri', async () => {
-  logger.info(`Daily FTP job scheduler launched! Date: ${new Date()}`);
-  await scheduler.launch();
-});
-cron.schedule('30 01 * * sat', async () => {
-  logger.info(`Daily FTP job scheduler launched! Date: ${new Date()}`);
-  await scheduler.launch();
-});
-cron.schedule('30 01 * * sun', async () => {
-  logger.info(`Daily FTP job scheduler launched! Date: ${new Date()}`);
-  await scheduler.launch();
-});
 
 server.listen(process.env.PORT || port, () => 
   logger.info('Node Express server for ' + app.name + ' listening on http://localhost:' + port));
